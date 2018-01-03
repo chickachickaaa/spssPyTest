bb713471368f6440ac11bbe51b48398219b5de37
Keira <higgins.taylor.a@gmail.com>
2018-01-03 00:15:09 -0500

    need IBM SPSS Statistics installed
    After IBM SPSS Statistics is installed. The applications start IDLE from the Python location that is specified on the File Locations tab in the Options dialog. You can then use import spss to start driving the IBM SPSS Statistics processor.

--- pythonSample.py
+++ pythonSample.py
@@ -1,2 +1,5 @@
 
 #here I will create a sample python script where we import SPSS library and interact with the SPSS data
+#must 
+import sys
+sys.path.insert(0,'<INSTALLDIR>/Python/lib/python2.7/site-packages')
