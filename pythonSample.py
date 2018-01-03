70c094a23510995c03a46894670727024bd494d0
Keira <higgins.taylor.a@gmail.com>
2018-01-03 00:16:42 -0500

    Update and rename pythonSample.py to sitecustomize.py

--- pythonSample.py
+++
@@ -1,5 +0,0 @@
-
-#here I will create a sample python script where we import SPSS library and interact with the SPSS data
- 
-import sys
-sys.path.insert(0,'<INSTALLDIR>/Python/lib/python2.7/site-packages')
---
+++ sitecustomize.py
@@ -0,0 +1,3 @@
+
+import sys
+sys.path.insert(0,'<INSTALLDIR>/Python/lib/python2.7/site-packages')
